661ca2ecc9e86a01b9c2db52edb427dd9777ade4
safaaamro <safaaamro072@gmail.com>
2019-02-10 14:51:50 +0200

    update jasonpackege

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(router);
 
-// build mode
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
