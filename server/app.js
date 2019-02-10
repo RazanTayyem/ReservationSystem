d2f1f3c5b84118df5b3f2059a585a8dd2127c922
safaaamro <safaaamro072@gmail.com>
2019-02-10 14:30:51 +0200

    setting heroku

--- server/app.js
+++ server/app.js
@@ -5,20 +5,11 @@ const router = require('./router');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(router);
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, 'client/build/index.html'));
-  });
-}
 // build mode
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
 app.set('port', process.env.PORT || 3002);
