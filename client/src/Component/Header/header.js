661d11278d665855c634831a77796a22bda583d6
safaaamro <safaaamro072@gmail.com>
2019-02-10 16:00:32 +0200

    make header

---
+++ client/src/Component/Header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+
+const Header = () => (
+  <div className="header">
+    <div className="nav-bar"></div>
+  </div>
+);
+export default Header;
