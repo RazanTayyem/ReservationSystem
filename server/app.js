368561d274cd01fbeba7f107198469df1556a27c
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-11 11:04:49 +0200

    deleteing comments

--- server/app.js
+++ server/app.js
@@ -17,11 +17,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
-// app.get('/:city/:street', (request, response) => {
-//   const city = request.params.city;
-//   const street = request.params.street;
-//   response.send('Hello ' + city[0].toUpperCase() + city.slice(1) + '<br>' + street);
-// })
-
 app.set('port', process.env.PORT || 3002);
 module.exports = app;
