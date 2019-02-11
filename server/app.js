7a0865c8587da52ea76ec78ae875e8355c7cfbe1
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-11 09:50:56 +0200

    installing compression module

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const router = require('./router');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(compression());
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
