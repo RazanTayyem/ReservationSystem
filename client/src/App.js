b88685cc5d78cf73e958ed09753b27722e0acbcc
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-12 15:31:18 +0200

    editing the path /events to direct it to Hall component instead of Calendar itself

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-import Login from './components/Login/login';
+import Login from "./components/Login/login";
 import Calendar from "./components/Calendar";
 import BookEvent from "./components/BookEvent";
 import DetailsEvent from "./components/DetailsEvent";
 import Error from "./components/Error";
-
-
+import Hall from "./components/Hall";
 
 class App extends Component {
   render() {
@@ -16,11 +15,11 @@ class App extends Component {
       <Router>
         <React.Fragment>
           <Route exact path="/" component={Login} />
-          <Route exact path="/events" component={Calendar} />
+          <Route exact path="/events" component={Hall} />
           <Route exact path="/bookevent" component={BookEvent} />
           <Route exact path="/detailsevent" component={DetailsEvent} />
           <Route exact path="/error" component={Error} />
-      </React.Fragment>
+        </React.Fragment>
       </Router>
     );
   }
