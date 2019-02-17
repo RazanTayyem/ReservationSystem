9ff8842c445caae743bef2c23ae56c859fc0fc98
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-18 00:41:18 +0200

    handling the PUT /event:id endpoint inside the router with auth middleware before getting to it

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const { login } = require('./controllers/login.js');
 const { logout } = require('./controllers/login.js');
 const { getEvents } = require('./controllers/getEvents.js');
 const { postEvent } = require('./controllers/postEvent.js');
+const { updateEvent } = require('./controllers/updateEvent.js');
 const { auth } = require('./middlewares/auth.js');
 
 const router = express.Router();
@@ -11,4 +12,5 @@ router.get('/events', getEvents);
 router.post('/login', login);
 router.get('/logout', logout);
 router.post('/event', auth, postEvent);
+router.put('/event/:id', auth, updateEvent);
 module.exports = router;
