144850838179a4e69dab5edb2771ab254a85dc3e
Amin <ayman201266@hotmail.com>
2019-02-17 09:34:19 +0200

    solve two bugs which add the -a -f to the query script and change the userID from 0 to 1 #42

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ insert into users (username, password, role)
   values ('ydrc','$2a$10$D/IX/AtYw5YHT4YWI2B2aOg5ZGMBodHNDx2x6vIbHWomyH4fsJ9SG','user');
 
 insert into events (title,start_date,end_date,org_name,price,capacity,note,"userId",status)
-  values ('Interview',DATE 'Thu, 14 February 2019 13:30:00', DATE 'Thu, 14 February 2019 13:30:00','YDRC',120,10,'The event is for the interview',0,1);
+  values ('Interview',DATE 'Thu, 14 February 2019 13:30:00', DATE 'Thu, 14 February 2019 13:30:00','YDRC',120,10,'The event is for the interview',1,1);
   insert into lunches (note,price,time,"eventId")
     values ('Makloba',30,'12:15',1)
   insert into coffees (note,price,time,"eventId")
@@ -13,7 +13,7 @@ insert into events (title,start_date,end_date,org_name,price,capacity,note,"user
     values ('Datashow',20,1)
 
 insert into events (title,start_date,end_date,org_name,price,capacity,note,"userId",status)
-  values ('Meeting',DATE 'Mon, 11 February 2019 13:30:00', DATE 'Mon, 11 February 2019 13:30:00','PPU',50,5,'The event is for the interview',0,1);
+  values ('Meeting',DATE 'Mon, 11 February 2019 13:30:00', DATE 'Mon, 11 February 2019 13:30:00','PPU',50,5,'The event is for the interview',2,1);
   insert into lunches (note,price,time,"eventId")
     values ('Kabsa',30,'12:30',2)
   insert into coffees (note,price,time,"eventId")
