e5c73aa60e76650c61cadc848fd894f2e3e5e01b
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-18 00:50:54 +0200

    removing console.log()

--- server/controllers/updateEvent.js
+++ server/controllers/updateEvent.js
@@ -1,7 +1,6 @@
 const { Event } = require('../database/models');
 
 exports.updateEvent = (req, res) => {
-  console.log(req.params.id);
   Event.update({ status: 1 }, { where: { id: req.params.id } })
     .then(() => res.json({ success: 'status updated' }))
     .catch(() => res.status(500).json({ error: 'something wrong in query ' }));
