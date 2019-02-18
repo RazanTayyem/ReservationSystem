9b9903b216169c8b6553238e7ad8592f4086082a
safaaamro <safaaamro072@gmail.com>
2019-02-18 15:59:52 +0200

    make style for form detalies

--- client/src/components/DetailsEvent/index.js
+++ client/src/components/DetailsEvent/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavBar from "../NavBar";
 import SideBar from "../SideBar";
 import axios from "axios";
+import "./detailsevent.css";
 
 class DetailsEvent extends Component {
   state = {
@@ -53,6 +54,7 @@ class DetailsEvent extends Component {
   };
 
   render() {
+    console.log("state", this.state);
     if (this.state.loading === true) {
       const {
         title = "event-title",
@@ -64,16 +66,16 @@ class DetailsEvent extends Component {
         note = ""
       } = this.state.event_details || {};
 
-      const { coffee_note = "", coffee_price = "", coffee_time = "" } = {
-        ...this.state.coffee_details
-      };
+      const coffee_note = "" || this.state.coffee_details.note;
+      const coffee_price = "" || this.state.coffee_details.price;
+      const coffee_time = "" || this.state.coffee_details.time;
 
-      const { lunch_note = "", lunch_price = "", lunch_time = "" } =
-        this.state.lunch_details || {};
+      const lunch_note = "" || this.state.lunch_details.note;
+      const lunch_price = "" || this.state.lunch_details.price;
+      const lunch_time = "" || this.state.lunch_details.time;
 
-      const { equipment_price = "", equipment_note = "" } =
-        this.state.equipment_details || {};
-
+      const equipment_price = "" || this.state.equipment_details.price;
+      const equipment_note = "" || this.state.equipment_details.note;
       let start_ddmmyyyy = new Date(start_date);
 
       start_ddmmyyyy =
@@ -102,38 +104,30 @@ class DetailsEvent extends Component {
               <form onSubmit={this.handleSubmitForm}>
                 <h2 className="h2">General</h2>
                 <div className="labels_container">
-                  <label className="title">Event title:</label>
+                  <label className="title">Event Name:</label>
                   <label className="answer">{title}</label>
-                </div>
-                <div className="labels_container">
-                  <label className="title">Organization name:</label>
+                  <label className="Organization">Organization name:</label>
                   <label className="answer">{org_name}</label>
-                </div>
-                <div className="labels_container">
-                  <label className="title">Hall cost:</label>
-                  <label className="answer">{price}</label>
-                </div>
-                <div className="labels_container">
-                  <label className="title">Number of persons:</label>
-                  <label className="answer">{capacity}</label>
                 </div>
                 <div className="labels_container">
                   <label className="title">Start Date:</label>
                   <label className="answer">{start_ddmmyyyy}</label>
-                </div>
-                <div className="labels_container">
-                  <label className="title">End Date:</label>
+                  <label className="EndDate">End Date:</label>
                   <label className="answer">{end_ddmmyyyy}</label>
                 </div>
                 <div className="labels_container">
-                  <label className="title">note:</label>
+                  <label className="title">Hall cost:</label>
+                  <label className="answer">{price}</label>
+                  <label className="persons">Number of persons:</label>
+                  <label className="answer">{capacity}</label>
+                </div>
+                <div className="labels_container">
+                  <label className="Note">note:</label>
                   <label className="answer">{note}</label>
                 </div>
+
                 <h2 className="h2">Lunch</h2>
-                <div className="labels_container">
-                  <label className="title">Description:</label>
-                  <label className="answer">{lunch_note}</label>
-                </div>
+
                 <div className="labels_container">
                   <label className="title">time:</label>
                   <label className="answer">{lunch_time}</label>
@@ -142,11 +136,11 @@ class DetailsEvent extends Component {
                   <label className="title">price per person:</label>
                   <label className="answer">{lunch_price}</label>
                 </div>
-                <h2 className="h2">Coffee</h2>
                 <div className="labels_container">
                   <label className="title">Description:</label>
-                  <label className="answer">{coffee_note}</label>
+                  <label className="answer">{lunch_note}</label>
                 </div>
+                <h2 className="h2">Coffee</h2>
                 <div className="labels_container">
                   <label className="title">time:</label>
                   <label className="answer">{coffee_time}</label>
@@ -155,20 +149,33 @@ class DetailsEvent extends Component {
                   <label className="title">price per person:</label>
                   <label className="answer">{coffee_price}</label>
                 </div>
-                <h2 className="h2">Equipment</h2>
                 <div className="labels_container">
                   <label className="title">Description:</label>
-                  <label className="answer">{equipment_note}</label>
+                  <label className="answer">{coffee_note}</label>
                 </div>
+                <h2 className="h2">Equipment</h2>
                 <div className="labels_container">
                   <label className="title">price:</label>
                   <label className="answer">{equipment_price}</label>
                 </div>
+                <div className="labels_container">
+                  <label className="title">Description:</label>
+                  <label className="answer">{equipment_note}</label>
+                </div>
                 {this.state.status && <input type="submit" value="OK" />}
-                {!this.state.status && <input type="submit" value="Approve" />}
-                {!this.state.status && (
-                  <input type="button" value="Cancel" onClick={this.cancel} />
-                )}
+                <div className="button">
+                  {!this.state.status && (
+                    <input type="submit" value="Approve" className="Approve" />
+                  )}
+                  {!this.state.status && (
+                    <input
+                      type="button"
+                      value="Cancel"
+                      onClick={this.cancel}
+                      className="Cancel"
+                    />
+                  )}
+                </div>
               </form>
             </div>
           </div>
