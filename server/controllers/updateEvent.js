9b47fc730a9da1d41466e0288bf475dc7a23fcb5
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-18 10:10:41 +0200

    handling the response depends on the query result so if theres something wrong the server will not crash

--- server/controllers/updateEvent.js
+++ server/controllers/updateEvent.js
@@ -2,6 +2,12 @@ const { Event } = require('../database/models');
 
 exports.updateEvent = (req, res) => {
   Event.update({ status: 1 }, { where: { id: req.params.id } })
-    .then(() => res.json({ success: 'status updated' }))
+    .then((result) => {
+      if (result) {
+        res.json({ success: 'status updated' });
+      } else {
+        res.json({ sucess: 'status did not get updated!' });
+      }
+    })
     .catch(() => res.status(500).json({ error: 'something wrong in query ' }));
 };
