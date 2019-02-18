5db45345376be8b1595b7a315c9f2e018001046c
Amin <ayman201266@hotmail.com>
2019-02-18 21:49:06 +0200

    make a homeLogo link to the home page which /events

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./NavBar.css";
 import logo from "./logo.png";
+import { Link } from 'react-router-dom'
 
 const NavBar = () => (
   <div className="nav-outer">
     <div className="nav-bar1">
-      <img className="logo" src={logo} alt="logo" />
+      <Link to="/events"><img className="logo" src={logo} alt="logo" /></Link>
       <button className="logout-btn">log out</button>
     </div>
   </div>
