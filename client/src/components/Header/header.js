b3e583222bf89736fcb4cac8f1b82234ba9aa056
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-20 15:17:47 +0200

    resized the photo and deleted all comments

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,11 +1,5 @@
 import React from "react";
 import "./header.css";
-// import logo from "./logo.png";
 
 const Header = () => <div className="header" />;
 export default Header;
-
-//<div className="nav-bar">
-//     <img src={logo} alt="logo" width="6%" height="40px"/>
-// <a href="url"className="link">logout</a>
-//     </div>
