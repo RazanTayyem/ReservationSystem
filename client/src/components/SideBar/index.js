be6ef2e8a4713d37408600e711c93adb87639474
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-21 13:21:24 +0200

    getting the details for the related service from recived props and changed the static data to be filled from what we got from the database, added design for pic too

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import "./sidebar.css";
-import hall1 from "./hall1.png";
 
-const SideBar = () => (
-  <div className="side-bar">
-    <div className="card">
-      <img src={hall1} alt="hall" />
-      <h3 className="card-typing">Co-working Space 1</h3>
-      <p className="card-typing">15 persons</p>
-      <p className="card-typing">300 &#8362;</p>
-      <p className="card-typing">data show</p>
-      <div className="agenda-colors">
-        <div className="color-typing">
-          <div className="blue" />
-          <p className="agenda-prop">approved</p>
-        </div>
-        <div className="color-typing">
-          <div className="yellow" />
-          <p className="agenda-prop">pending</p>
+const SideBar = props => {
+  const { service } = props.state;
+
+  return (
+    <div className="side-bar">
+      <div className="card">
+        <img src={service.image} alt="hall" className="imageCard" />
+        <h3 className="card-typing">{service.name}</h3>
+        <p className="card-typing">{service.capacity}</p>
+        <p className="card-typing">{service.equipment}</p>
+        <div className="agenda-colors">
+          <div className="color-typing">
+            <div className="blue" />
+            <p className="agenda-prop">approved</p>
+          </div>
+          <div className="color-typing">
+            <div className="yellow" />
+            <p className="agenda-prop">pending</p>
+          </div>
         </div>
       </div>
+      <div className="line" />
     </div>
-    <div className="line" />
-  </div>
-);
+  );
+};
 export default SideBar;
