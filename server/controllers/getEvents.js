21af6a0a6861a518403f5e907de21c928c4a9002
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-02-24 09:16:33 +0200

    add query to get the service details too

--- server/controllers/getEvents.js
+++ server/controllers/getEvents.js
@@ -2,6 +2,7 @@ const { Event, Service } = require('../database/models');
 
 exports.getEvents = (req, res) => {
   const { id } = req.params;
+  console.log('integer', id);
   Event.findAll({ where: { serviceId: id }, raw: true })
     .then((events) => {
       if (!events || events.length === 0) return res.status(401).json({ message: 'no data exists' });
@@ -9,5 +10,8 @@ exports.getEvents = (req, res) => {
         res.json({ service, events });
       });
     })
-    .catch(err => res.status(500).json({ message: err }));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
 };
